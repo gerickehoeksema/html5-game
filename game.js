5b4e3e9aaf1a9072979d84c342c07d95f3c765b4
Gericke <ghoeksema@thecodegroup.co.za>
2019-10-16 19:52:15 +0200

    -making player to be able to move around

--- game.js
+++ game.js
@@ -24,8 +24,8 @@ imgSprite.addEventListener("load", init, false);
 
 
 function init() {
-    //document.addEventListener("keydown", checkKeyDown, false);
-    //document.addEventListener("keyup", checkKeyUp, false);
+    document.addEventListener("keydown", function(e) {checkKey(e,true);}, false);
+    document.addEventListener("keyup", function(e) {checkKey(e,false);}, false);
     //defineObstacles();
     //initEnemies();
     begin();
@@ -39,7 +39,7 @@ function begin() {
 
 function update() {
     clearCtx(ctxEntities);
-    player1.draw();
+    player1.update();
 }
 
 function draw() {
@@ -72,11 +72,11 @@ function Player() {
     this.centerX = this.drawX + (this.width / 2); 
     this.centerY = this.drawY + (this.height / 2);
     this.speed = 2;
-    //this.isUpKey = false;
-    //this.isRightKey = false;
-    //this.isDownKey = false;
-    //this.isLeftKey = false;
-    //this.isSpacebar = false;
+    this.isUpKey = false;
+    this.isRightKey = false;
+    this.isDownKey = false;
+    this.isLeftKey = false;
+    this.isSpacebar = false;
     //this.isShooting = false;
 
     //var numBullets = 10;
@@ -90,11 +90,86 @@ function Player() {
 Player.prototype.update = function() {
     this.centerX = this.drawX + (this.width / 2); 
     this.centerY = this.drawY + (this.height / 2);
-    //this.checkDirection();
+    this.checkDirection();
     //this.checkShooting();
     //this.updateAllBullets();
 }
 
 Player.prototype.draw = function() {
     ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+}
+
+Player.prototype.checkDirection = function() {
+    var newDrawX = this.drawX,
+        newDrawY = this.drawY,
+        obstacleCollision = false;
+
+    if(this.isUpKey) {
+        newDrawY -= this.speed;
+        this.srcX = 35; // Facing north image
+    }
+    if(this.isDownKey) {
+        newDrawY += this.speed;
+        this.srcX = 0; // Facing south image
+    }
+    if(this.isRightKey) {
+        newDrawX += this.speed;
+        this.srcX = 105; // Facing east image
+    }
+    if(this.isLeftKey) {
+        newDrawX -= this.speed;
+        this.srcX = 70; // Facing west image
+    }
+
+    if(!obstacleCollision && !outOfBounds(this, newDrawX, newDrawY)) {
+        this.drawX = newDrawX;
+        this.drawY = newDrawY;
+    }
+}
+
+function checkKey(e, value) {
+    var kedID = e.keyCode || e.which; // if not supported
+    // up arrow
+    if(kedID === 38) {
+        player1.isUpKey = value;
+        e.preventDefault();
+    }
+    // right arrow
+    if(kedID === 39) {
+        player1.isRightKey = value;
+        e.preventDefault();
+    }
+    // down arrow
+    if(kedID === 40) {
+        player1.isDownKey = value;
+        e.preventDefault();
+    }
+    // left arrow
+    if(kedID === 37) {
+        player1.isLeftKey = value;
+        e.preventDefault();
+    }
+    // sapcebar
+    if(kedID === 32) {
+        player1.isSpacebar = value;
+        e.preventDefault();
+    }
+}
+
+// Player object not going out of the borders 
+function outOfBounds(a,x,y) { 
+    // a=object,x=object x postition, y=object y position
+    var newBottomY = y + a.height,
+        newTopY = y,
+        newRightX = x + a.width,
+        newLeftX = x,
+        treeLineTop = 5,
+        treeLineBottom = 570,
+        treeLineRight = 750,
+        treeLineLeft = 65;
+        ;
+        return newBottomY > treeLineBottom ||
+            newTopY < treeLineTop ||
+            newRightX > treeLineRight ||
+            newLeftX < treeLineLeft;
 }
